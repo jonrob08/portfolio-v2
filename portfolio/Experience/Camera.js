366e0f82c5652ad72a6eb62bbf53ef06b19f6ba8
jonrob <jonrob08@gmail.com>
2023-01-07 09:55:59 -0700

    chore: added 2 camera types for easier pathing later

--- portfolio/Experience/Camera.js
+++ portfolio/Experience/Camera.js
@@ -1,11 +1,38 @@
-import Experience from "./Experience.js"
+import Experience from "./Experience.js";
 
+// The Camera class represents a camera in the 3D scene. It has a constructor method that is called when a new instance of the class is created.
 export default class Camera {
-    constructor(){
-        this.experience = new Experience()
-        this.sizes = this.experience.sizes
-        this.scene = this.experience.scene
-        this.canvas = this.experience.canvas
-        console.log(this.sizes, this.scene, this.canvas)
-    }
+  constructor() {
+    // creates a new instance of Experience and assigns it to this
+    this.experience = new Experience();
+    // assign sizes, scene, and canvas to the experience
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.canvas = this.experience.canvas;
+
+    // two camera types are useful for when I start pathing
+    this.createPerspectiveCamera();
+    this.createOrthographicCamera();
+  }
+
+  createPerspectiveCamera() {
+    this.perspectiveCamera = new THREE.PerspectiveCamera(
+      35,
+      this.sizes.aspect,
+      0.1,
+      1000
+    );
+    this.scene.add(this.perspectiveCamera);
+  }
+  createOrthographicCamera() {
+    this.frustrum = 5;
+    this.orthographicCamera = new THREE.OrthographicCamera(
+      (-this.sizes.aspect * this.sizes.frustrum) / 2,
+      (this.sizes.aspect * this.sizes.frustrum) / 2,
+      this.sizes.frustrum / 2,
+      -this.sizes.frustrum / 2,
+      -100,
+    );
+    this.scene.add(this.orthographicCamera);
+  }
 }
