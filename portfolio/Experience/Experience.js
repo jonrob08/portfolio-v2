47e302f6ac7020ecff85fd43ab4d285b5e82d269
jonrob <jonrob08@gmail.com>
2023-01-07 10:56:37 -0700

    chore: setting up event emitter in time file

--- portfolio/Experience/Experience.js
+++ portfolio/Experience/Experience.js
@@ -1,6 +1,7 @@
 import * as THREE from "three"
 
 import Sizes from "./Utils/Sizes"
+import Time from "./Utils/Time"
 
 import Camera from "./Camera"
 import Renderer from "./Renderer"
@@ -14,6 +15,7 @@ export default class Experience {
         Experience.instance = this
         this.canvas = canvas
         this.scene = new THREE.Scene()
+        this.time = new Time()
         this.sizes = new Sizes()
         this.camera = new Camera()
         this.renderer = new Renderer()
