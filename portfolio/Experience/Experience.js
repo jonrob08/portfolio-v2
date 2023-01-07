e1196093b00cf942eb8a0afc20ed4f3530aa76ae
jonrob <jonrob08@gmail.com>
2023-01-07 09:44:45 -0700

    chore: Camera.js is working with Experience and Sizes now

--- portfolio/Experience/Experience.js
+++ portfolio/Experience/Experience.js
@@ -1,5 +1,8 @@
 import * as THREE from "three"
+
 import Sizes from "./Utils/Sizes"
+
+import Camera from "./Camera"
 
 export default class Experience {
     static instance
@@ -11,5 +14,6 @@ export default class Experience {
         this.canvas = canvas
         this.scene = new THREE.Scene()
         this.sizes = new Sizes()
+        this.camera = new Camera()
     }
 }
