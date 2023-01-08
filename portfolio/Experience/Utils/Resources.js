424a956ee9df9e5da886750b10064db50e077fb0
jonrob <jonrob08@gmail.com>
2023-01-08 15:15:27 -0700

    chore: seeing asset files loaded in the app now

--- portfolio/Experience/Utils/Resources.js
+++ portfolio/Experience/Utils/Resources.js
@@ -1,3 +1,4 @@
+import * as THREE from "three";
 import { EventEmitter } from "events"
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
 import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader"
@@ -23,10 +24,52 @@ export default class Resources extends EventEmitter{
 
     setLoaders(){
         this.loaders = {}
+        this.loaders.gltfLoader = new GLTFLoader()
+        this.loaders.dracoLoader = new DRACOLoader()
+        this.loaders.dracoLoader.setDecoderPath("/draco/")
+        this.loaders.gltfLoader.setDRACOLoader(this.loaders.dracoLoader)
     }
 
     startLoading(){
+        for(const asset of this.assets){
+            if (asset.type === "glbModel") {
+                this.loaders.gltfLoader.load(asset.path, (file) => {
+                    this.singleAssetLoaded(asset, file)
+                })
+            } else if (asset.type === "videoTexture") {
+                this.video = {}
+                this.videoTexture = {}
 
+                this.video[asset.name] = document.createElement("video")
+                this.video[asset.name].src = asset.path
+                this.video[asset.name].muted = true
+                this.video[asset.name].playsInline = true
+                this.video[asset.name].autoplay = true
+                this.video[asset.name].loop = true
+                this.video[asset.name].play()
+
+                this.videoTexture[asset.name] = new THREE.VideoTexture(
+                    this.video[asset.name]
+                )
+                this.videoTexture[asset.name].flipY = true
+                this.videoTexture[asset.name].minFilter = THREE.NearestFilter
+                this.videoTexture[asset.name].magFilter = THREE.NearestFilter
+                this.videoTexture[asset.name].generateMipmaps = false
+                this.videoTexture[asset.name].encoding = THREE.sRGBEncoding
+            }
+        }
+    }
+
+    singleAssetLoaded(asset, file){
+        this.items[asset.name] = file
+        this.loaded++
+
+        console.log(file)
+        
+        if (this.loaded === this.queue){
+            // Create world when assets are loaded
+            this.emit("ready")
+        }
     }
 
 }
