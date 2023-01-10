efc8b151af1cfe23023c06fa7beffc5541a4f40a
jonrob <jonrob08@gmail.com>
2023-01-10 04:11:20 -0700

    chore: messed around with the camera enough, I'm going to actually start on the office first and do some HTML stuff

--- portfolio/Experience/World/Office.js
+++ portfolio/Experience/World/Office.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import Experience from "../Experience";
+import GSAP from "gsap";
 
 export default class Office {
   constructor() {
@@ -10,8 +11,15 @@ export default class Office {
     this.office = this.resources.items.office
     this.actualOffice = this.office.scene
 
+    this.lerp = {
+      current: 0,
+      target: 0,
+      ease: 0.1,
+    };
+
     this.setModel()
     this.setAnimation()
+    this.onMouseMove()
   }
 
   setModel(){
@@ -55,9 +63,25 @@ export default class Office {
     this.swim.play()
   }
 
+  onMouseMove(){
+    window.addEventListener("mousemove", (e) => {
+      // console.log(e)
+      this.rotation = ((e.clientX - window.innerWidth / 2) * 2) / window.innerWidth
+      this.lerp.target = this.rotation * 0.1
+    })
+  }
+
   resize() {}
 
   update() {
+    this.lerp.current = GSAP.utils.interpolate(
+      this.lerp.current,
+      this.lerp.target,
+      this.lerp.ease
+    );
+
+    this.actualOffice.rotation.y = this.lerp.current
+
     this.mixer.update(this.time.delta)
   }
 }
