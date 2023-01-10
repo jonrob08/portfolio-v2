e22c1718a6aabb20639eead510519d959991cbe8
jonrob <jonrob08@gmail.com>
2023-01-10 06:27:06 -0700

    chore: tested moving mesh on the x axis, now going to add scroll trigger

--- portfolio/Experience/World/Controls.js
+++ portfolio/Experience/World/Controls.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import Experience from "../Experience";
 import GSAP from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default class Controls {
   constructor() {
@@ -9,6 +10,19 @@ export default class Controls {
     this.resources = this.experience.resources;
     this.time = this.experience.time;
     this.camera = this.experience.camera;
+    this.office = this.experience.world.office.actualOffice
+    GSAP.registerPlugin(ScrollTrigger)
+
+    this.setPath()
+  }
+
+  setPath(){
+    console.log(this.office)
+    this.timeline = new GSAP.timeline()
+    this.timeline.to(this.office.position, {
+        x: 5,
+        duration: 20,
+    })
   }
 
   resize() {}
