32a668f814e0ce9b0b51c538ae76310a5c045d6e
jonrob <jonrob08@gmail.com>
2023-01-09 17:07:15 -0700

    chore: pulled in example curve from threejs docs

--- portfolio/Experience/World/Controls.js
+++ portfolio/Experience/World/Controls.js
@@ -14,12 +14,25 @@ export default class Controls {
 
 
   setPath() {
+    const curve = new THREE.CatmullRomCurve3([
+        new THREE.Vector3( -10, 0, 10 ),
+        new THREE.Vector3( -5, 0, 5 ),
+        new THREE.Vector3( 0, 0, 0 ),
+        new THREE.Vector3( 5, -5, 5 ),
+        new THREE.Vector3( 10, 0, 10 ),
+    ])
+
+    const points = curve.getPoints( 50 )
+    const geometry = new THREE.BufferGeometry().setFromPoints( points )
+
+    const material = new THREE.LineBasicMaterial( { color: 0xff0000 } )
+
+    const curveObject = newThree.Line( geometry, material )
 
   }
 
   resize() {}
 
   update() {
-    this.mixer.update(this.time.delta)
   }
 }
