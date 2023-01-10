9fcce5803e068434843cfd9cb57d26846ead6a5b
jonrob <jonrob08@gmail.com>
2023-01-09 17:41:42 -0700

    chore: got orthographic camera set up on the curve, tested getPointAt method of vector copying

--- portfolio/Experience/World/Controls.js
+++ portfolio/Experience/World/Controls.js
@@ -4,35 +4,41 @@ import Experience from "../Experience";
 export default class Controls {
   constructor() {
     this.experience = new Experience();
-    this.scene = this.experience.scene
-    this.resources = this.experience.resources
-    this.time = this.experience.time
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+    this.time = this.experience.time;
+    this.camera = this.experience.camera
 
-    this.setPath()
+    this.setPath();
   }
 
-
-
   setPath() {
-    this.curve = new THREE.CatmullRomCurve3([
-        new THREE.Vector3( -10, 0, 10 ),
-        new THREE.Vector3( -5, 0, 5 ),
-        new THREE.Vector3( 0, 0, 0 ),
-        new THREE.Vector3( 5, -5, 5 ),
-        new THREE.Vector3( 10, 0, 10 ),
-    ])
+    this.curve = new THREE.CatmullRomCurve3(
+      [
+        new THREE.Vector3(-10, 0, 10),
+        new THREE.Vector3(-5, 0, 5),
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(5, -5, 5),
+        new THREE.Vector3(10, 0, 10),
+      ],
+      true
+    );
 
-    const points = this.curve.getPoints( 50 )
-    const geometry = new THREE.BufferGeometry().setFromPoints( points )
+    this.testVector = new THREE.Vector3(0,0,0)
+    this.curve.getPointAt(1, this.testVector)
+    console.log(this.testVector)
+    this.camera.orthographicCamera.position.copy(this.testVector)
 
-    const material = new THREE.LineBasicMaterial( { color: 0xff0000 } )
+    const points = this.curve.getPoints(50);
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-    const curveObject = new THREE.Line( geometry, material )
-    this.scene.add(curveObject)
+    const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+
+    const curveObject = new THREE.Line(geometry, material);
+    this.scene.add(curveObject);
   }
 
   resize() {}
 
-  update() {
-  }
+  update() {}
 }
