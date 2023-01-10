97d9d5298fc8896b38b76363f617b58c149e81fd
jonrob <jonrob08@gmail.com>
2023-01-10 10:31:20 -0700

    chore: added lil gui to have a color picker which controls the lighting color

--- portfolio/Experience/World/Environment.js
+++ portfolio/Experience/World/Environment.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import Experience from "../Experience";
 import GSAP from "gsap"
+import GUI from "lil-gui"
 
 export default class Environment {
   constructor() {
@@ -8,8 +9,29 @@ export default class Environment {
     this.scene = this.experience.scene
     this.resources = this.experience.resources
 
+    this.gui = new GUI()
+    this.obj = {
+        colorObj: {
+            r: 0,
+            g: 0,
+            b: 0
+       },
+       intensity: 3,
+    }
 
     this.setSunlight()
+    this.setGUI()
+  }
+
+  setGUI(){
+    this.gui.addColor(this.obj, "colorObj").onChange(() => {
+        this.sunlight.color.copy(this.obj.colorObj)
+        this.ambientLight.color.copy(this.obj.colorObj)
+    })
+    this.gui.add(this.obj, "intensity", 0, 10).onChange(() => {
+        this.sunlight.intensity = this.obj.intensity
+        this.ambientLight.intensity = this.obj.intensity
+    })
   }
 
   setSunlight(){
