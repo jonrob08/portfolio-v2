52b3f54963c1996e174094b5ceffb20de49cd24c
jonrob <jonrob08@gmail.com>
2023-01-09 17:23:38 -0700

    bug: added a helper to orthographic camera in order to visibily see it and fixed an issue with the frustrum

--- portfolio/Experience/Camera.js
+++ portfolio/Experience/Camera.js
@@ -22,11 +22,12 @@ export default class Camera {
       1000
     );
     this.scene.add(this.perspectiveCamera);
-    this.perspectiveCamera.position.z = 5
+    this.perspectiveCamera.position.x = 29
+    this.perspectiveCamera.position.y = 14
+    this.perspectiveCamera.position.z = 12
   }
 
   createOrthographicCamera() {
-    this.frustrum = 5;
     this.orthographicCamera = new THREE.OrthographicCamera(
       (-this.sizes.aspect * this.sizes.frustrum) / 2,
       (this.sizes.aspect * this.sizes.frustrum) / 2,
@@ -35,8 +36,10 @@ export default class Camera {
       -100,
       100
     );
-
+    
     this.scene.add(this.orthographicCamera);
+    this.helper = new THREE.CameraHelper(this.orthographicCamera)
+    this.scene.add(this.helper)
 
     const size = 10
     const divisions = 10
@@ -66,6 +69,13 @@ export default class Camera {
   }
 
   update() {
+    // console.log(this.perspectiveCamera.position)
     this.controls.update()
+
+    this.helper.matrixWorldNeedsUpdate = true
+    this.helper.update()
+
+    this.helper.position.copy(this.orthographicCamera.position)
+    this.helper.rotation.copy(this.orthographicCamera.rotation)
   }
 }
