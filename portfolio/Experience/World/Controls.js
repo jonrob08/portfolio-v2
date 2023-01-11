56a46c487d9ae0cd46fa3542a378faa97efdca60
jonrob <jonrob08@gmail.com>
2023-01-11 13:29:59 -0700

    chore: added first and second scroll trigger moves

--- portfolio/Experience/World/Controls.js
+++ portfolio/Experience/World/Controls.js
@@ -20,14 +20,53 @@ export default class Controls {
   setScrollTrigger(){
     ScrollTrigger.matchMedia({
         // Desktop timeline
-        "(min-width: 969px)": function() {
+        "(min-width: 969px)": () => {
             console.log("desktop view")
+            // First Section (About Me)
+            this.firstMoveTimeline = new GSAP.timeline({
+                scrollTrigger: {
+                    trigger: ".first-move",
+                    start: "top top",
+                    end: "bottom bottom",
+                    scrub: 0.6,
+                    invalidateOnRefresh: true,
+                }
+            })
+            this.firstMoveTimeline.to(this.office.position,{
+                x: () => {
+                    return this.sizes.width * 0.0014
+                }
+            })
+
+            // Second Section (Projects)
+            this.secondMoveTimeline = new GSAP.timeline({
+                scrollTrigger: {
+                    trigger: ".second-move",
+                    start: "top top",
+                    end: "bottom bottom",
+                    scrub: 0.6,
+                    invalidateOnRefresh: true,
+                }
+            })
+            this.secondMoveTimeline.to(this.office.position,{
+                x: () => {
+                    return 1
+                },
+                z: () => {
+                    return this.sizes.height * 0.0032
+                }
+            }, "same")
+            this.secondMoveTimeline.to(this.office.scale,{
+                x: 0.3,
+                y: 0.3,
+                z: 0.3
+            }, "same")
         },  
         // Mobile timeline
-        "(min-width: 968px)": function() {
+        "(min-width: 968px)": () => {
             console.log("mobile view")
         },  
-        "all": function() {
+        "all": () => {
     
         }
           
